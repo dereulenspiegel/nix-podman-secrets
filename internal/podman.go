238789818bb0fc78f154bc93d0bc1b533cd136b4
dereulenspiegel <till.klocke@gmail.com>
2024-10-03 15:03:48 +0200

    Removing unnecessary single quotation marks

--- internal/podman.go
+++ internal/podman.go
@@ -51,7 +51,7 @@ func createPodmanSecret(secretName string) error {
 		"create",
 		"--label", "source=nix",
 		"--driver", "shell",
-		"--driver-opts", fmt.Sprintf("delete='%s-delete',list='%s-list',lookup='%s-lookup',store='%s-store'",
+		"--driver-opts", fmt.Sprintf("delete=%s-delete,list=%s-list,lookup=%s-lookup,store=%s-store",
 			nixPodmanSecretsBin,
 			nixPodmanSecretsBin,
 			nixPodmanSecretsBin,
